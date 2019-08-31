2330c77c50a12e99e0b2a271048897082f64b655
Stivenescobar22 <estibenes@gmail.com>
2019-08-31 15:44:40 -0500

    se agregan archivos faltantes del proyecto

---
+++ promedio_estudiante/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace promedio_estudiante
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btncalculo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblnota1 = new System.Windows.Forms.Label();
+            this.lblnota2 = new System.Windows.Forms.Label();
+            this.txtnota1 = new System.Windows.Forms.TextBox();
+            this.txtnota2 = new System.Windows.Forms.TextBox();
+            this.txtnota3 = new System.Windows.Forms.TextBox();
+            this.lblnota3 = new System.Windows.Forms.Label();
+            this.lblpromedio = new System.Windows.Forms.Label();
+            this.lblprom = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btncalculo
+            // 
+            this.btncalculo.Location = new System.Drawing.Point(318, 261);
+            this.btncalculo.Name = "btncalculo";
+            this.btncalculo.Size = new System.Drawing.Size(75, 23);
+            this.btncalculo.TabIndex = 0;
+            this.btncalculo.Text = "calculo";
+            this.btncalculo.UseVisualStyleBackColor = true;
+            this.btncalculo.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(259, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "promedio estudiante";
+            // 
+            // lblnota1
+            // 
+            this.lblnota1.AutoSize = true;
+            this.lblnota1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnota1.Location = new System.Drawing.Point(238, 75);
+            this.lblnota1.Name = "lblnota1";
+            this.lblnota1.Size = new System.Drawing.Size(50, 20);
+            this.lblnota1.TabIndex = 2;
+            this.lblnota1.Text = "nota1";
+            // 
+            // lblnota2
+            // 
+            this.lblnota2.AutoSize = true;
+            this.lblnota2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnota2.Location = new System.Drawing.Point(238, 119);
+            this.lblnota2.Name = "lblnota2";
+            this.lblnota2.Size = new System.Drawing.Size(54, 20);
+            this.lblnota2.TabIndex = 3;
+            this.lblnota2.Text = "nota 2";
+            // 
+            // txtnota1
+            // 
+            this.txtnota1.Location = new System.Drawing.Point(318, 75);
+            this.txtnota1.Name = "txtnota1";
+            this.txtnota1.Size = new System.Drawing.Size(100, 20);
+            this.txtnota1.TabIndex = 4;
+            // 
+            // txtnota2
+            // 
+            this.txtnota2.Location = new System.Drawing.Point(318, 119);
+            this.txtnota2.Name = "txtnota2";
+            this.txtnota2.Size = new System.Drawing.Size(100, 20);
+            this.txtnota2.TabIndex = 5;
+            // 
+            // txtnota3
+            // 
+            this.txtnota3.Location = new System.Drawing.Point(318, 160);
+            this.txtnota3.Name = "txtnota3";
+            this.txtnota3.Size = new System.Drawing.Size(100, 20);
+            this.txtnota3.TabIndex = 6;
+            // 
+            // lblnota3
+            // 
+            this.lblnota3.AutoSize = true;
+            this.lblnota3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnota3.Location = new System.Drawing.Point(238, 163);
+            this.lblnota3.Name = "lblnota3";
+            this.lblnota3.Size = new System.Drawing.Size(54, 20);
+            this.lblnota3.TabIndex = 7;
+            this.lblnota3.Text = "nota 3";
+            // 
+            // lblpromedio
+            // 
+            this.lblpromedio.AutoSize = true;
+            this.lblpromedio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblpromedio.Location = new System.Drawing.Point(224, 206);
+            this.lblpromedio.Name = "lblpromedio";
+            this.lblpromedio.Size = new System.Drawing.Size(88, 20);
+            this.lblpromedio.TabIndex = 8;
+            this.lblpromedio.Text = "Promedio : ";
+            // 
+            // lblprom
+            // 
+            this.lblprom.AutoSize = true;
+            this.lblprom.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblprom.Location = new System.Drawing.Point(337, 206);
+            this.lblprom.Name = "lblprom";
+            this.lblprom.Size = new System.Drawing.Size(27, 20);
+            this.lblprom.TabIndex = 9;
+            this.lblprom.Text = "##";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblprom);
+            this.Controls.Add(this.lblpromedio);
+            this.Controls.Add(this.lblnota3);
+            this.Controls.Add(this.txtnota3);
+            this.Controls.Add(this.txtnota2);
+            this.Controls.Add(this.txtnota1);
+            this.Controls.Add(this.lblnota2);
+            this.Controls.Add(this.lblnota1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btncalculo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btncalculo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblnota1;
+        private System.Windows.Forms.Label lblnota2;
+        private System.Windows.Forms.TextBox txtnota1;
+        private System.Windows.Forms.TextBox txtnota2;
+        private System.Windows.Forms.TextBox txtnota3;
+        private System.Windows.Forms.Label lblnota3;
+        private System.Windows.Forms.Label lblpromedio;
+        private System.Windows.Forms.Label lblprom;
+    }
+}
+
---
+++ promedio_estudiante/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace promedio_estudiante
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+
+            string not1 = txtnota1.Text;
+            string not2 = txtnota2.Text;
+            string not3 = txtnota3.Text;
+            string prom = lblprom.Text;
+
+            float nota1 = float.Parse(not1);
+            float nota2 = float.Parse(not2);
+            float nota3 = float.Parse(not3);
+            float promedio = (nota1 + nota2 + nota3) / 3;
+
+            lblprom.Text = promedio.ToString();
+
+        }
+    }
+}
